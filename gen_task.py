d61b187adf04ddb8292b27e625dab946671c53a8
vkir26 <vanya.kireew2012@yandex.ru>
2025-05-14 20:22:21 +0300

    add app

---
+++ gen_task.py
@@ -0,0 +1,22 @@
+import random
+
+
+class Task:
+    def __init__(self):
+        self.task = generate_task()
+
+    def generate(self):
+        return self.task
+
+class Answer:
+    ...
+
+def generate_task() -> list[int]:
+    return sorted([random.randint(1, 5) for _ in range(2)])
+
+def answer_validator(ready_task: Task, answer: Answer) -> bool:
+    ...
+
+if __name__ == "__main__":
+    task = Task()
+    print(task.generate())
