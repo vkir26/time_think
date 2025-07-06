1032dc22c39cad61255ad68263decba1a58183fc
vkir26 <vanya.kireew2012@yandex.ru>
2025-07-06 16:08:25 +0300

    add answer_validator.py

--- gen_task.py
+++ gen_task.py
@@ -1,24 +1,57 @@
-import random
+from dataclasses import dataclass
+import pytest
 
 
+@dataclass(frozen=True, slots=True)
 class Task:
-    def __init__(self):
-        self.task = generate_task()
-
-    def generate(self):
-        return self.task
+    task: str
+    correct_answer: int
 
 
-class Answer: ...
+@dataclass(frozen=True, slots=True)
+class Answer:
+    user_answer: int
 
 
 def generate_task() -> list[int]:
-    return sorted([random.randint(1, 5) for _ in range(2)])
+    return [1, 1]
 
 
-def answer_validator(ready_task: Task, answer: Answer) -> bool: ...
+def get_task() -> Task:
+    task_values = generate_task()
+    task_condition = "{} + {}".format(*task_values)
+    correct_answer = sum(task_values)
+
+    return Task(task=task_condition, correct_answer=correct_answer)
+
+
+def answer_validator(ready_task: Task, answer: Answer) -> bool:
+    if ready_task.correct_answer == answer.user_answer:
+        return True
+    return False
 
 
 if __name__ == "__main__":
-    task = Task()
-    print(task.generate())
+    task = get_task()
+    attempts = 5
+    for i in range(attempts):
+        print(task.task)
+        user_answer = int(input())
+        if answer_validator(ready_task=task, answer=Answer(user_answer)):
+            print("Correct")
+            break
+        else:
+            print("Not correct")
+
+
+@pytest.mark.parametrize(
+    "ready_task, answer, result",
+    [(1, 1, True), (1, 2, False), (5, 3, False), (7, 7, True)],
+)
+def test_answer_validator(ready_task: int, answer: int, result: bool) -> None:
+    assert (
+        answer_validator(
+            Task(task="None", correct_answer=ready_task), Answer(user_answer=answer)
+        )
+        == result
+    )
