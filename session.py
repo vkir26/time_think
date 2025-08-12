e86589e99b6c2f8d5676886b59e12e933a028d86
vkir26 <vanya.kireew2012@yandex.ru>
2025-08-12 22:17:25 +0300

    Added the first test for the game session

--- session.py
+++ session.py
@@ -1,22 +1,46 @@
-from core import main, get_task
+from enum import StrEnum
+
+from core import main, get_task, Task
+
+
+class Message(StrEnum):
+    END_GAME = "Игра завершилась, Ваши результаты:"
+    CORRECT = "Correct"
+    NOT_CORRECT = "Not correct"
+    USER_INPUT_ERROR = "Ответ должен быть числом.\nПовторите попытку ввода: "
+
+
+def user_answer() -> int:
+    try:
+        return int(input())
+    except ValueError:
+        print(Message.USER_INPUT_ERROR)
+        return user_answer()
+
+
+def check_task(generated_task: Task) -> bool:
+    return main(generated_task, user_answer())
 
 
 def run() -> str:
     attempts = 5
-    wrong_answer = 0
+    not_correct_answer = 0
     correct_answer = 0
 
     task = get_task()
     for i in range(attempts):
-        if wrong_answer != 3:
+        if not_correct_answer != 3:
             print(task)
-            user_answer = int(input())
-            if main(task, user_answer):
-                print("Correct")
+            if check_task(task):
+                print(Message.CORRECT)
                 correct_answer += 1
                 task = get_task()
             else:
-                print("Not correct")
-                wrong_answer += 1
+                print(Message.NOT_CORRECT)
+                not_correct_answer += 1
 
-    return f"Correct: {correct_answer}\nWrong: {wrong_answer}"
+    return (
+        f"{'=' * 15}\n{Message.END_GAME}\n"
+        f"{Message.CORRECT}: {correct_answer}\n"
+        f"{Message.NOT_CORRECT}: {not_correct_answer}"
+    )
