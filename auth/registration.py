5ec54fcaf16c9d2e96febb114224fe06cd51162d
vkir26 <vanya.kireew2012@yandex.ru>
2026-07-16 20:22:53 +0300

    Refactoring the Identity Data Catalog

--- auth/registration.py
+++ auth/registration.py
@@ -1,7 +1,6 @@
 import uuid
 from app.database import connect_db, Request
-from auth.config import peppered_password
-from argon2 import PasswordHasher
+from auth.config import peppered_password, PASSWORD_HASHER
 
 
 def name_is_exist(name: str) -> bool:
@@ -14,8 +13,7 @@ def name_is_exist(name: str) -> bool:
 
 def register(username: str, password: str) -> str:
     user_id = str(uuid.uuid4())
-    ph = PasswordHasher()
-    hashed_password = ph.hash(peppered_password(password))
+    hashed_password = PASSWORD_HASHER.hash(peppered_password(password))
     request = Request(
         query=""" INSERT INTO users (id, username, password) VALUES (?, ?, ?); """,
         param=(user_id, username, hashed_password),
